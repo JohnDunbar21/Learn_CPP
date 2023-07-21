9988e6475d4fb3e56dbe16f24f7d7974c82873ac
John Dunbar <u08jd21@abdn.ac.uk>
2023-07-21 20:53:56 +0100

    Updated vectors project.

--- Vectors/Projects/Whale_Talk.cpp
+++ Vectors/Projects/Whale_Talk.cpp
@@ -9,21 +9,22 @@ Create a program that takes a string and removes all consonants except the lette
 int main() {
 
     std::string user_string;
-    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-    std::vector<char> translation;
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    std::string translation;
 
     std::cout << "Enter a string to be translated into Whale Talk: ";
-    std::cin >> user_string;
+    std::getline(std::cin, user_string);
 
     for (int i = 0; i < user_string.size(); i++) {
         for (int j = 0; j < vowels.size(); j++) {
             if (user_string[i] == vowels[j]) {
                 translation.push_back(user_string[i]);
-                if (user_string[i] == 'u' || user_string[i] == 'e') {
+                if (user_string[i] == 'u' || user_string[i] == 'U' || user_string[i] == 'e' || user_string[i] == 'E') {
                     translation.push_back(user_string[i]);
                 }
             }
         }
     }
-
+  
+    std::cout << "The translation is: " << translation << "\n";
 }
